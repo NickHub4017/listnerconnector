95fcb87ae8adc97f98ae179d4ee07b0dc2ce5c60
NickHub4017 <nrv.rox.u@gmail.com>
2016-11-03 22:27:18 +0530

    Add OutputDeamon, And OutputTest server, TestOutputDataProducer

--- sqliteinterupt/testcases/testproducer.py
+++ sqliteinterupt/testcases/testproducer.py
@@ -1,11 +1,12 @@
 import os
 import sys
 import random
+import time
 
 path = "/home/nrv/PycharmProjects/listnerconnector/outpipe"
 while(1):
     fifo = open(path,'w')
-
+    time.sleep(2)
     fifo.write(""+str(random.random()))
     fifo.close()
 
