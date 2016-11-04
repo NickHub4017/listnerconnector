3b76a7c52e907b745449488426f71fcf69ebeebd
NickHub4017 <nrv.rox.u@gmail.com>
2016-11-04 22:29:42 +0530

    Add Pid store - update - retriew functionality

--- sqliteinterupt/testcases/testcontrolserver2.py
+++ sqliteinterupt/testcases/testcontrolserver2.py
@@ -3,7 +3,7 @@ import socket
 import json
 import time
 TCP_IP = '127.0.0.1'
-TCP_PORT = 8090
+TCP_PORT = 8050
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,9 +17,9 @@ while(1):
         #if not data: break
         #print "received data:", data
     data={}
-    data['inpdeamon']={"ip":"127.0.0.1","port":8090,"name":'inpdeamon',"type":"client","protocol":"tcp"}
+    data['inpdeamon']={"ip":"127.0.0.1","port":8050,"name":'inpdeamon',"type":"client","protocol":"tcp"}
     data["oupdeamon"]={"ip":"127.0.0.1","port":8100,"name":'oudeamon',"type":"client","protocol":"tcp"}
-    data["cntrldeamon"]={"ip":"127.0.0.1","port":8090,"name":'cntrldeamon',"type":"client","protocol":"tcp"}
+    data["cntrldeamon"]={"ip":"127.0.0.1","port":8080,"name":'cntrldeamon',"type":"client","protocol":"tcp"}
 
     data["timestamp"]="2016-10-07"
     data["fromip"]="127.0.0.1"
@@ -32,5 +32,5 @@ while(1):
     conn.send(json.dumps(data))  # echo
     time.sleep(10)
     data["cntrldeamon"]["port"]=8080
-    conn.send(json.dumps(data))  # echo
+    #conn.send(json.dumps(data))  # echo
     #conn.close()
