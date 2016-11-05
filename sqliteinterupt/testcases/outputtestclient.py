f1696ae140e9df94d3bd625820212c5ae0b21c4c
NickHub4017 <nrv.rox.u@gmail.com>
2016-11-05 22:25:54 +0530

    Add TestClient for OutPut Server

---
+++ sqliteinterupt/testcases/outputtestclient.py
@@ -0,0 +1,27 @@
+import socket
+import time
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 8100
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World!"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.connect((TCP_IP, TCP_PORT))
+    while (True):
+        try:
+            print "Wait to receve"
+            msg=s.recv(BUFFER_SIZE)
+            print str(msg)
+
+            #time.sleep(0.5)
+        except Exception as E:
+            print E
+            break
+    #data = s.recv(BUFFER_SIZE)
+    s.close()
+except Exception as E:
+    print E.message
+
+#print "received data:", data
